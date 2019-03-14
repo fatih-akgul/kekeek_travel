6cadf158e6b0d77ba3d8d9806b352e907332cedf
fatih.akgul <akguls@gmail.com>
2019-03-14 13:37:45 -0400

    Initial commit

---
+++ src/main/java/com/kekeek/travel/KekeekTravelApplication.java
@@ -0,0 +1,14 @@
+package com.kekeek.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KekeekTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KekeekTravelApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/kekeek/kekeek_travel/KekeekTravelApplicationTests.java
@@ -0,0 +1,17 @@
+package com.kekeek.kekeek_travel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KekeekTravelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
